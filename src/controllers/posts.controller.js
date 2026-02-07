5d8fec78866811d91b4629f07fbb3ce3e20792aa
its-me-rbk <rubhak222@gmail.com>
2026-02-07 10:57:19 +0530

    feat/get-post-by-id-endpoint

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -11,4 +11,12 @@ const getAllPosts = (req, res) => {
   });
 };
 
-module.exports = {getAllPosts};
+const getPostById = (req, res) => {
+  const postId = req.params.postId;
+
+  res.status(200).json({
+    message: `Fetching data for post with ID:  ${postId}`
+  });
+};
+
+module.exports = {getAllPosts, getPostById};
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postController = require('../controllers/posts.controller.js');
 
 router.get('/', postController.getAllPosts);
+router.get('/:postId', postController.getPostById);
 
 module.exports = router;
