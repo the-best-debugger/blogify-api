89f7e9a7f0ce19f59d7652a229fdd3f4d51d7729
its-me-rbk <rubhak222@gmail.com>
2026-02-12 11:39:09 +0530

    Made json envelope in posts

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,6 +2,7 @@ const getSingleUser = (req, res) => {
   const requestedUserId = req.params.userId;
 
   res.status(200).json({
+    success: true,
     message: `You requested data for User ID: ${requestedUserId}`
   });
 };
