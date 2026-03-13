c386b72e82b6e0e7de22894a37f979d677c2e27b
its-me-rbk <rubhak222@gmail.com>
2026-03-13 10:18:15 +0530

    Embedded author using reference in post schema

--- models/posts.js
+++ models/posts.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  author: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
